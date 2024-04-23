7d90fbb87d9a8e55225f74cf45735458f51dca05
박예지 <gksquf517@naver.com>
2024-04-23 18:00:31 +0900

    [Init] #frontend 프로젝트 생성

---
+++ front_wear/android/app/src/main/kotlin/com/example/front_wear/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.front_wear
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ front_wear/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:wear_plus/wear_plus.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: WatchShape(
+            builder: (context, shape, child) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Shape: ${shape == WearShape.round ? 'round' : 'square'}',
+                  ),
+                  child!,
+                ],
+              );
+            },
+            child: AmbientMode(
+              builder: (context, mode, child) {
+                return Text(
+                  'Mode: ${mode == WearMode.active ? 'Active' : 'Ambient'}',
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
