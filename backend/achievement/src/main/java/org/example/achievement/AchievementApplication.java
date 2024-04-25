9d86955ac174822fe91c7949236342eda5b85524
박정호 <cuzzzu1318@naver.com>
2024-04-26 00:16:09 +0900

    [Init] # back 서비스 추가

---
+++ backend/achievement/src/main/java/org/example/achievement/AchievementApplication.java
@@ -0,0 +1,13 @@
+package org.example.achievement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchievementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchievementApplication.class, args);
+	}
+
+}
