76359af55d3f1825a70bcb9037e61d5c47ccfff2
박정호 <cuzzzu1318@naver.com>
2024-04-29 17:43:52 +0900

    [Feat] 사용자 정보 수정 / 조회

---
+++ back/src/main/java/org/example/back/member/service/MemberServiceImpl.java
@@ -0,0 +1,66 @@
+package org.example.back.member.service;
+
+import org.example.back.db.entity.Member;
+import org.example.back.db.repository.MemberRepository;
+import org.example.back.exception.MemberNotFoundException;
+import org.example.back.member.dto.ProfileResponseDto;
+import org.example.back.member.dto.ProfileUpdateRequestDto;
+import org.example.back.util.SecurityUtil;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+
+	private final SecurityUtil securityUtil;
+	private final MemberRepository memberRepository;
+
+	@Override
+	public ProfileResponseDto findById() {
+
+		Member member = getMember();
+
+		return ProfileResponseDto.builder()
+			.nickname(member.getNickname())
+			.weight(member.getWeight())
+			// TODO: 캐릭터 아이디 바꿔 줘야함.
+			.characterId(1L)
+			.build();
+	}
+
+	@Override
+	public ProfileResponseDto updateProfile(ProfileUpdateRequestDto profileUpdateRequestDto) {
+
+		// 현재 로그인중인 유저
+		Member member = getMember();
+
+		// 입력받은 닉네임이 있으면 업데이트
+		if (profileUpdateRequestDto.getNickname() != null) {
+			member.updateNickname(profileUpdateRequestDto.getNickname());
+		}
+		
+		//  입력받은 몸무게가 있으면 업데이트
+		if (profileUpdateRequestDto.getWeight() != null) {
+			member.updateWeight(profileUpdateRequestDto.getWeight());
+		}
+
+		memberRepository.save(member);
+
+		return ProfileResponseDto.builder()
+			.nickname(member.getNickname())
+			.weight(member.getWeight())
+			// TODO: 캐릭터 아이디 바꿔 줘야함.
+			.characterId(1L)
+			.build();
+	}
+
+	// security 정보에서 사용자 정보 가져오기
+	private Member getMember() {
+		Long id = SecurityUtil.getCurrentMemberId();
+
+		Member member = memberRepository.findById(id).orElseThrow(MemberNotFoundException::new);
+		return member;
+	}
+}
