2126ff5c1fe7b7949c68c0e5f141d9acd718272c
최도훈 <chlehgns123@naver.com>
2024-04-29 12:31:28 +0900

    [Feat] Secure Storage 토큰 저장 및 조회 코드 추가

---
+++ front_android/lib/util/secure_storage.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorage with ChangeNotifier {
+  AndroidOptions _getAndroidOptions() => const AndroidOptions(
+        encryptedSharedPreferences: true,
+      );
+  late final storage = FlutterSecureStorage(aOptions: _getAndroidOptions());
+
+  Future<String?> get accessToken async => await storage.read(
+        key: 'accessToken',
+      );
+
+  Future<String?> get refreshToken async =>
+      await storage.read(key: 'refreshToken');
+
+  Future<DateTime> get refreshTokenExpireDate async => DateTime.parse(
+      await storage.read(key: 'expireDate') ?? '2000-01-01T12:00:00.000Z');
+
+  Future<void> setAccessToken(String token) async {
+    await storage.write(key: 'accessToken', value: token);
+  }
+
+  Future<void> setRefreshToken(String token, DateTime expireDate) async {
+    List<Future> asyncTasks = [
+      storage.write(key: 'refreshToken', value: token),
+      storage.write(key: 'expireDate', value: expireDate.toIso8601String()),
+    ];
+
+    await Future.wait(asyncTasks);
+  }
+}
