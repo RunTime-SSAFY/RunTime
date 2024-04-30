3d3a71368a58e8ba595ab43ec9923afc798ab64b
최도훈 <chlehgns123@naver.com>
2024-05-01 01:30:51 +0900

    [Feat] 위치 정보 패키지 변경

--- front_android/macos/Flutter/GeneratedPluginRegistrant.swift
+++ front_android/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,13 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import flutter_secure_storage_macos
-import location
+import geolocator_apple
 import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
-  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
