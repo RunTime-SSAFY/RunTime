7152268d27f28afeb6d0320468d56ecbd473b336
박정호 <cuzzzu1318@naver.com>
2024-05-01 15:14:40 +0900

    [Feat] 친구 리스트(작업중)

---
+++ back/src/main/java/org/example/back/friend/controller/FriendController.java
@@ -0,0 +1,11 @@
+package org.example.back.friend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/friends")
+public class FriendController {
+
+
+}
