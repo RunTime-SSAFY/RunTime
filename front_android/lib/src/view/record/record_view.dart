e035df380f5d1b7d72aadfe786afca1ca16121a6
HYEONMIN LEE <hyunmin2667@gmail.com>
2024-05-02 00:45:29 +0900

    [Fix] AppBarMain, AppBarSub 삭제

--- front_android/lib/src/view/record/record_view.dart
+++ front_android/lib/src/view/record/record_view.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:front_android/src/service/theme_service.dart';
+import 'package:front_android/theme/components/bottom_navigation.dart';
+import 'package:front_android/theme/components/svg_icon.dart';
 import 'package:front_android/util/lang/generated/l10n.dart';
 
 class RecordView extends ConsumerWidget {
@@ -9,13 +13,22 @@ class RecordView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
+      backgroundColor: Colors.transparent,
       appBar: AppBar(
         title: Text(
           S.current.record,
           style: ref.typo.appBarMainTitle,
         ),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 20),
+            child: SvgIcon(
+              'bell',
+            ),
+          ),
+        ],
       ),
-      body: Container(),
+      bottomNavigationBar: const BottomNavigationWidget(),
     );
   }
 }
