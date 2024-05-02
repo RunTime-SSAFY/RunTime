7427c99114e130c65157f6d7590ee4a644bbe622
박정호 <cuzzzu1318@naver.com>
2024-05-02 18:03:33 +0900

    [!HOTFIX] 브랜치 복구

--- back/src/main/java/org/example/back/db/entity/Member.java
+++ back/src/main/java/org/example/back/db/entity/Member.java
@@ -6,6 +6,8 @@ import org.hibernate.annotations.ColumnDefault;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -33,7 +35,8 @@ public class Member extends BaseEntity {
 
 	@Lob
 	@Column(name = "role")
-	private String role;
+	@Enumerated(EnumType.STRING)
+	private RoleType role;
 
 	@Column(name = "nickname", length = 30)
 	private String nickname;
