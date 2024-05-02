f4b90b1c7cdfaf74788f657cc688f0f207dd2613
박정호 <cuzzzu1318@naver.com>
2024-05-02 16:18:51 +0900

    Revert "[Fix] master merge"
    This reverts commit c7f8270f74752f1c1acc1d8770bfc0e07f7a9400

---
+++ back/src/main/java/org/example/back/auth/kakao/service/KakaoServiceImpl.java
@@ -0,0 +1,10 @@
+package org.example.back.auth.kakao.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class KakaoServiceImpl implements KakaoService{
+}
