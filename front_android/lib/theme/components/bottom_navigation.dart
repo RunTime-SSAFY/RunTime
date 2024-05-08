7616acd48865bc5ca23d8a0bf68dd6fc14ce8195
leehyeonmin <hyunmin2667@gmail.com>
2024-05-08 17:19:35 +0900

    [Feat] 내비게이션 아이콘 수정

--- front_android/lib/theme/components/bottom_navigation.dart
+++ front_android/lib/theme/components/bottom_navigation.dart
@@ -1,35 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:front_android/src/service/theme_service.dart';
 import 'package:front_android/util/lang/generated/l10n.dart';
 
-class BottomNavigationWidget extends StatelessWidget {
+class BottomNavigationWidget extends ConsumerWidget {
   const BottomNavigationWidget({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int currentIndex = ref.watch(bottomNavigationProvider);
+
     return BottomNavigationBar(
       type: BottomNavigationBarType.fixed,
       items: [
         BottomNavigationBarItem(
-          icon: const Icon(Icons.home),
+          icon: const Icon(Icons.home_outlined),
+          activeIcon: const Icon(Icons.home),
           label: S.current.home,
         ),
         BottomNavigationBarItem(
           icon: const Icon(Icons.article_outlined),
+          activeIcon: const Icon(Icons.article),
           label: S.current.achievement,
         ),
         BottomNavigationBarItem(
-          icon: const Icon(Icons.verified),
+          icon: const Icon(Icons.star_border_outlined),
+          activeIcon: const Icon(Icons.star),
           label: S.current.character,
         ),
         BottomNavigationBarItem(
-          icon: const Icon(Icons.timer),
+          icon: const Icon(Icons.timer_outlined),
+          activeIcon: const Icon(Icons.timer),
           label: S.current.record,
         ),
         BottomNavigationBarItem(
-          icon: const Icon(Icons.account_circle_rounded),
+          icon: const Icon(Icons.account_circle_outlined),
+          activeIcon: const Icon(Icons.account_circle),
           label: S.current.profile,
         ),
       ],
+      currentIndex: currentIndex,
+      selectedItemColor: ref.color.accept,
     );
   }
 }
+
+final bottomNavigationProvider = StateProvider<int>((ref) => 0);
