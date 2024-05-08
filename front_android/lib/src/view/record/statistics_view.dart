20978b5959785f6a9c048ad392a7a07466617501
leehyeonmin <hyunmin2667@gmail.com>
2024-05-08 18:10:07 +0900

    [Feat] 통계화면 카드 생성

--- front_android/lib/src/view/record/statistics_view.dart
+++ front_android/lib/src/view/record/statistics_view.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -82,15 +84,126 @@ class StatisticsList extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Center();
+    return Expanded(
+      child: ListView(
+        children: [
+          StatisticsCard(),
+        ],
+      ),
+    );
   }
 }
 
 class StatisticsCard extends ConsumerWidget {
   const StatisticsCard({super.key});
+  // 데이터
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      // 실제 보이는 카드 모양 시작
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [
+                ref.palette.gray900,
+                ref.palette.gray800,
+              ],
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 2,
+                offset: const Offset(1, 2),
+              ),
+            ]),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // 타이틀
+              Text('전체통계',
+                  style: ref.typo.headline1.copyWith(color: ref.color.white)),
+              // 달린일수, 총 칼로리
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    flex: 5,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('달린 일수',
+                            style: ref.typo.subTitle4
+                                .copyWith(color: ref.palette.gray600)),
+                        Text('448 일',
+                            style: ref.typo.headline1
+                                .copyWith(color: ref.color.white)),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 5,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('총 칼로리',
+                            style: ref.typo.subTitle4
+                                .copyWith(color: ref.palette.gray600)),
+                        Text('80,361 kcal',
+                            style: ref.typo.headline1
+                                .copyWith(color: ref.color.white)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              // 총 거리, 총 시간
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    flex: 5,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('총 거리',
+                            style: ref.typo.subTitle4
+                                .copyWith(color: ref.palette.gray600)),
+                        Text('8,872 km',
+                            style: ref.typo.headline1
+                                .copyWith(color: ref.color.white)),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 5,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('총 시간',
+                            style: ref.typo.subTitle4
+                                .copyWith(color: ref.palette.gray600)),
+                        Text('5일 3시간',
+                            style: ref.typo.headline1
+                                .copyWith(color: ref.color.white)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
