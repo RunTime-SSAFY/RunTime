cc50007f3c87af5cb12f98ffc920186551e9e79a
최도훈 <chlehgns123@naver.com>
2024-05-09 14:55:19 +0900

    [Feat] Stomp 연결 성공

--- front_android/lib/src/repository/stomp_repository.dart
+++ front_android/lib/src/repository/stomp_repository.dart
@@ -2,19 +2,47 @@ import 'dart:convert';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:front_android/src/service/auth_service.dart';
 import 'package:stomp_dart_client/stomp_dart_client.dart';
 
-final stompInstanceProvider = Provider.autoDispose((ref) => StompRepository());
+final stompInstanceProvider = Provider.autoDispose((ref) {
+  var stompRepository = StompRepository();
+  ref.onDispose(() {
+    print('연결 종료');
+    stompRepository.disconnect();
+  });
+  return stompRepository;
+});
 
 class StompRepository {
   late StompClient _client;
 
   StompRepository() {
+    print('생성: StompRepository ${dotenv.get('SOCKET_URL')}');
     _client = StompClient(
-      config: StompConfig(
-        url: dotenv.get('WEBSOCKET_URL'),
-        onConnect: (StompFrame stompFrame) {
-          print('웹 소켓 서버 연결 완료');
+      config: StompConfig.sockJS(
+        url: dotenv.get('SOCKET_URL'),
+        stompConnectHeaders: {
+          'Authorization': AuthService.instance.accessToken!,
+          'Connection': 'upgrade'
+        },
+        webSocketConnectHeaders: {
+          'Authorization': AuthService.instance.accessToken!,
+        },
+        onConnect: (p0) {
+          print('웹 소켓 연결 성공 ${p0.toString()}');
+        },
+        onDebugMessage: (p0) {
+          print('StompClient 내부 디버그 메세지${p0.toString()}');
+        },
+        onWebSocketError: (p0) {
+          print('소켓 연결 에러 ${p0.toString()}');
+        },
+        onUnhandledMessage: (p0) {
+          print('핸들러가 없는 이벤트${p0.toString()}');
+        },
+        onDisconnect: (p0) {
+          print('웹 소켓 서버 연결 해제 완료');
         },
       ),
     )..activate();
@@ -50,6 +78,5 @@ class StompRepository {
 
   void disconnect() {
     _client.deactivate();
-    print('웹 소켓 서버 연결 해제 완료');
   }
 }
