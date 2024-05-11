8b5917467f1f4d088e38e890892859f088989771
최도훈 <chlehgns123@naver.com>
2024-05-11 14:45:12 +0900

    [Feat] 매칭 화면 애니메이션 추가

--- front_android/lib/src/view/matching/waiting_matching_view.dart
+++ front_android/lib/src/view/matching/waiting_matching_view.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:front_android/src/service/theme_service.dart';
 import 'package:front_android/src/view/matching/matching_view_model.dart';
+import 'package:front_android/src/view/matching/widgets/lodinag_animation.dart';
 import 'package:front_android/src/view/matching/widgets/matching_layout.dart';
 import 'package:front_android/theme/components/button.dart';
-import 'package:front_android/theme/components/svg_icon.dart';
 import 'package:front_android/util/lang/generated/l10n.dart';
 import 'package:go_router/go_router.dart';
 
@@ -41,11 +41,12 @@ class WaitingMatching extends ConsumerWidget {
           backGroundColor: ref.color.deny,
           fontColor: ref.color.onDeny,
         ),
-        middleWidget: SvgIcon(
-          'matching/waitingIcon',
-          color: ref.color.onBackground,
-          size: 100,
-        ),
+        middleWidget: const LoadingAnimatedBars(),
+        // SvgIcon(
+        //   'matching/waitingIcon',
+        //   color: ref.color.onBackground,
+        //   size: 100,
+        // ),
       ),
     );
   }
