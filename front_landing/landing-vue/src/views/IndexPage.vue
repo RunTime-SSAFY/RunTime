5e77fc79f24b34a5eec79a080e8d8e2d9244fca3
Jo-dv <whgjsgh11@gmail.com>
2024-05-14 17:53:54 +0900

    [Feat] 랜딩 페이지 개발

---
+++ front_landing/landing-vue/src/views/IndexPage.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+    <section>
+        <h1>Run Time</h1>
+    </section>
+    <section>
+        <img src="./src/assets/qrcode.png" alt="">
+        <h1>QR</h1>
+    </section>
+    <section>
+        <button>Download</button>
+    </section>
+</template>
+
+<style scoped>
+
+</style>
