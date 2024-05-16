2408d2748b24ff60fdec75cfe447925e18f79d1d
leehyeonmin <hyunmin2667@gmail.com>
2024-05-16 18:08:59 +0900

    [Feat] 기록 api 연결 중

--- front_android/lib/src/repository/record_repository.dart
+++ front_android/lib/src/repository/record_repository.dart
@@ -1,23 +1,37 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:front_android/src/model/record_list_model.dart';
-import 'package:front_android/src/model/record_model.dart';
+import 'package:front_android/src/model/record.dart';
+import 'package:front_android/src/model/record_list_response.dart';
 import 'package:front_android/src/service/https_request_service.dart';
 
 class RecordRepository {
-  final dio = apiInstance;
-  late RecordListModel recordListModel;
+  var api = apiInstance;
 
-  Future<RecordModel> getRecordList() async {
+  bool hasNext = true;
+  int lastId = 0;
+  List<Record> recordList = [];
+
+  RecordListResponse recordListResponse = RecordListResponse();
+
+  Future<void> fetchRecordList(
+      int pageSize, int lastId, String gameMode) async {
     try {
-      var response = await dio.get("/records?size=10&lastId=0");
-      final json = jsonDecode(response.data) as Map<String, dynamic>;
-      debugPrint(json.toString());
-      return RecordModel.fromJson(json);
+      final response = await api.get("/records", queryParameters: {
+        "pageSize": pageSize,
+        "lastId": lastId,
+        "gameMode": gameMode,
+      });
+      print("--------[RecordRepository] fetchRecordList --------");
+      print(response.data);
+      recordListResponse = RecordListResponse.fromJson(response.data);
+      // recordList에 계속 추가
+      recordList.addAll(recordListResponse.recordList!);
+      hasNext = recordListResponse.hasNext!;
+      this.lastId = recordListResponse.lastId!;
+      print("--------[RecordRepository] fetchRecordList --------");
+      print(recordListResponse);
     } catch (e, s) {
       debugPrint('에러 발생 $e, $s');
-      return RecordModel.fromJson({});
+      throw Error();
     }
   }
 }
