bbf71461711ebf676f61ab70ac36eb2975e46969
crcho5133 <140131170+crcho5133@users.noreply.github.com>
2024-05-17 11:22:40 +0900

    [Feat] #back 회원가입 테스트 로직, Builder.default 추가

---
+++ back/src/main/java/org/example/back/auth/dto/TestDto.java
@@ -0,0 +1,12 @@
+package org.example.back.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TestDto {
+    private String email;
+}
