048d777f860d96245af4a035ce0ddb56e22c49f8
최도훈 <chlehgns123@naver.com>
2024-05-18 20:53:26 +0900

    [Feat] 연습 모드 준비

--- front_android/lib/src/view/practice/practice_view.dart
+++ front_android/lib/src/view/practice/practice_view.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:front_android/src/service/theme_service.dart';
+import 'package:front_android/src/view/practice/practice_view_model.dart';
 import 'package:front_android/src/view/practice/widget/summary_box.dart';
 import 'package:front_android/theme/components/button.dart';
 import 'package:front_android/theme/components/image_background.dart';
 import 'package:front_android/theme/components/png_image.dart';
+import 'package:front_android/util/helper/route_path_helper.dart';
 import 'package:front_android/util/lang/generated/l10n.dart';
 import 'package:go_router/go_router.dart';
 
@@ -13,6 +15,8 @@ class Practice extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    PracticeViewModel viewModel = ref.watch(practiceViewModelProvider);
+
     return BattleImageBackground(
       child: Scaffold(
         appBar: AppBar(
@@ -48,7 +52,10 @@ class Practice extends ConsumerWidget {
                 const SummaryBox(),
                 const Spacer(flex: 2),
                 Button(
-                  onPressed: () {},
+                  onPressed: () {
+                    viewModel.startPractice();
+                    context.go(RoutePathHelper.battle);
+                  },
                   text: S.current.practiceStart,
                   backGroundColor: ref.color.accept,
                   fontColor: ref.color.onAccept,
