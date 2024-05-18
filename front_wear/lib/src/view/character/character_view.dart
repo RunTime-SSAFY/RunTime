2da7677f896f0b403f454a55e945119750ef26d5
박예지 <gksquf517@naver.com>
2024-05-18 14:09:38 +0900

    [Feat] character 이미지, 이름

--- front_wear/lib/src/view/character/character_view.dart
+++ front_wear/lib/src/view/character/character_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:front_wear/src/view/character/character_info.dart';
 
 class CharacterView extends StatefulWidget {
   const CharacterView({super.key});
@@ -29,27 +30,85 @@ class _CharacterViewState extends State<CharacterView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('캐릭터 선택'),
-      ),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4,
-          childAspectRatio: 0.75,
+        title: const Text(
+          '캐릭터',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 26,
+          ),
         ),
-        itemCount: characters.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: Image.asset(characters[index]['image']!),
-                ),
-                Text(characters[index]['name']!),
-              ],
+      ),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              '보유중2/20',
+              style: TextStyle(
+                fontSize: 16,
+              ),
             ),
-          );
-        },
+          ),
+          Expanded(
+            // Expanded 위젯 추가
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                childAspectRatio: 0.65,
+              ),
+              itemCount: characters.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const CharacterInfo(),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      minimumSize: const Size(100, 162),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(top: 12.0),
+                          child: Center(
+                            child: SvgPicture.asset('assets/icons/lock.svg'),
+                          ),
+                        ),
+                        Expanded(
+                          child: Image.asset(
+                            characters[index]['image']!,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 12.0),
+                          child: Text(
+                            characters[index]['name']!,
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
