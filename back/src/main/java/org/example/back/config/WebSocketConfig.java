6aedf2381e252947330d99fa9ff3d8deb6ae2dec
crcho5133 <140131170+crcho5133@users.noreply.github.com>
2024-05-18 16:15:12 +0900

    [Refactor] #back sockJS 추가

--- back/src/main/java/org/example/back/config/WebSocketConfig.java
+++ back/src/main/java/org/example/back/config/WebSocketConfig.java
@@ -19,8 +19,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*"); // TODO 특정 출처만 허용
-//                .withSockJS(); //  이거 있으면 socket 연결이 안 된다: 왜?
+                .setAllowedOriginPatterns("*") // TODO 특정 출처만 허용
+                .withSockJS(); //  이거 있으면 socket 연결이 안 된다: 왜?
         registry.setErrorHandler(stompErrorHandler);
 
     }
