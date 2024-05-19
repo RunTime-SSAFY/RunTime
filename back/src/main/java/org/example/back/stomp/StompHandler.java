cc80859f3c3d12d4be7648d21fea474e508039fe
crcho5133 <140131170+crcho5133@users.noreply.github.com>
2024-05-20 03:44:02 +0900

    [Refactor] 단체전에서의 접속을 stomp로 관리

--- back/src/main/java/org/example/back/stomp/StompHandler.java
+++ back/src/main/java/org/example/back/stomp/StompHandler.java
@@ -3,12 +3,16 @@ package org.example.back.stomp;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.back.db.entity.Member;
 import org.example.back.db.entity.Room;
 import org.example.back.db.entity.RoomMember;
 import org.example.back.db.repository.RoomMemberRepository;
 import org.example.back.db.repository.RoomRepository;
 import org.example.back.exception.RoomMemberNotFoundException;
 import org.example.back.exception.RoomNotFoundException;
+import org.example.back.room.dto.MemberResDto;
+import org.example.back.room.dto.StompExitRoomResDto;
+import org.example.back.room.dto.StompResDto;
 import org.example.back.room.service.RoomService;
 import org.example.back.util.JWTUtil;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,6 +20,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageDeliveryException;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
@@ -99,6 +104,45 @@ public class StompHandler implements ChannelInterceptor {
 
                 Long roomId = roomMember.getRoom().getId();
                 Room room = roomRepository.findById(roomId).orElseThrow(() -> new RoomNotFoundException(roomId));
+
+                Member me = roomMember.getMember();
+                Long myMemberId = me.getId();
+
+                room.getRoomMembers().remove(roomMember);
+
+                // 내가 방장이고 방을 나갔을 경우
+                // 남은 사람이 아무도 없는 경우: 방을 제거한다, 방의 멤버들의 등수도 redis에서 제거한다.
+                // 남은 사람이 1명 이상인 경우: 방장을 변경한다:
+                if (room.getManager().getId().equals(myMemberId)) {
+                    if (roomMembers.isEmpty()) {
+                        roomRepository.delete(room);
+                        redisTemplate.delete("uuid_roomId:" + roomId); // uuid 삭제
+                        redisTemplate.delete("ranking_roomId:" + roomId); // 방의 인원들의 등수를 담은 데이터를 삭제한다.
+
+                    }
+
+                    else {
+                        roomMembers.sort((r1, r2) -> r1.getCreatedAt().compareTo(r2.getCreatedAt()));
+                        room.setManager(roomMembers.get(0).getMember());
+                        roomMembers.get(0).setReady();
+                    }
+                }
+
+                //  게임 시작 전인 경우 socket으로 방의 멤버들에게 멤버들의 리스트를 보내준다
+                String uuid = redisTemplate.opsForValue().get("uuid_roomId:" + roomId);
+                if (!roomMembers.isEmpty() && room.getStatus().name().equals("WAITING")) {
+                    List<MemberResDto> memberResDtos = roomMembers.stream().map(RoomMember::toMemberResDto).toList();
+                    memberResDtos.get(0).setManager();
+
+                    StompResDto stompResDto = StompResDto.builder()
+                            .action("member").data(memberResDtos).build();
+
+                }
+
+                // 게임 진행 중인 경우 socket으로 방의 멤버들에게 나간 멤버의 id를 알려준다
+                else if (!roomMembers.isEmpty() && room.getStatus().name().equals("IN_PROGRESS")) {
+                    StompExitRoomResDto stompExitRoomResDto = StompExitRoomResDto.builder().nickname(me.getNickname()).build();
+                }
 
                 if (room.getRoomMembers().isEmpty()) {
                     roomRepository.delete(room);
