4b5bcd1985cc2d6d40948ec403ee54c15725db4c
crcho5133 <140131170+crcho5133@users.noreply.github.com>
2024-05-20 09:18:09 +0900

    [Refactor] #back 방 삭제 scheduler 다시 활성화

--- back/src/main/java/org/example/back/room/DeleteRoomScheduler.java
+++ back/src/main/java/org/example/back/room/DeleteRoomScheduler.java
@@ -1,27 +1,27 @@
-//package org.example.back.room;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.example.back.db.entity.Room;
-//import org.example.back.db.repository.RoomRepository;
-//import org.springframework.scheduling.annotation.Scheduled;
-//import org.springframework.stereotype.Component;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.List;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class DeleteRoomScheduler {
-//    private final RoomRepository roomRepository;
-//    @Scheduled(fixedDelay = 1000 * 10)
-//    @Transactional
-//    public void deleteRoom() {
-//        List<Room> rooms = roomRepository.findAll();
-//
-//        for (Room r: rooms) {
-//            if (r.getRoomMembers() == null || r.getRoomMembers().isEmpty()) {
-//                roomRepository.deleteById(r.getId());
-//            }
-//        }
-//    }
-//}
+package org.example.back.room;
+
+import lombok.RequiredArgsConstructor;
+import org.example.back.db.entity.Room;
+import org.example.back.db.repository.RoomRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class DeleteRoomScheduler {
+    private final RoomRepository roomRepository;
+    @Scheduled(fixedDelay = 1000 * 10)
+    @Transactional
+    public void deleteRoom() {
+        List<Room> rooms = roomRepository.findAll();
+
+        for (Room r: rooms) {
+            if (r.getRoomMembers() == null || r.getRoomMembers().isEmpty()) {
+                roomRepository.deleteById(r.getId());
+            }
+        }
+    }
+}
